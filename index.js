702cb89d19aeec842a2387aaa946267b72afe205
moonahmed <mdmoonahmedkhan10@gmail.com>
2025-11-10 22:57:31 +0600

    cars crud added

--- index.js
+++ index.js
@@ -26,13 +26,94 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db = client.db("model-db");
-    const modelCollection = db.collection("models");
-
-        app.get("/models", async (req, res) => {
-      const result = await modelCollection.find().toArray();
+    const db = client.db("car-db");
+    const carCollection = db.collection("cars");
+        
+    // all cars
+        app.get("/cars", async (req, res) => {
+      const result = await carCollection.find().toArray();
       res.send(result);
     });
+
+    //  car
+     app.get("/cars/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+
+      const result = await carCollection.findOne({ _id: objectId });
+
+      res.send({
+        result,
+      });
+    });
+
+     
+    // add car
+     app.post("/cars",   async (req, res) => {
+      const data = req.body;
+      // console.log(data)
+      const result = await carCollection.insertOne(data);
+      res.send({
+        result,
+      });
+    });
+
+
+    //  update car
+     app.put("/cars/:id",  async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      // console.log(id)
+      // console.log(data)
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+      const update = {
+        $set: data,
+      };
+
+      const result = await carCollection.updateOne(filter, update);
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+        
+    //   delete car
+      app.delete("/cars/:id",  async (req, res) => {
+      const { id } = req.params;
+      //    const objectId = new ObjectId(id)
+      // const filter = {_id: objectId}
+      const result = await carCollection.deleteOne({ _id: new ObjectId(id) });
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    //   latest car
+     app.get("/latest-cars", async (req, res) => {
+      const result = await carCollection
+        .find()
+        .sort({ created_at: "desc" })
+        .limit(6)
+        .toArray();
+
+      console.log(result);
+
+      res.send(result);
+    });
+
+
+
+    //   get my car
+     app.get("/my-cars", async(req, res) => {
+      const email = req.query.email
+      const result = await carCollection.find({created_by: email}).toArray()
+      res.send(result)
+    })
 
 
   } finally {
